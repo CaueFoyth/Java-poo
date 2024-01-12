7d015536eb3e6bb087741054a5fe33a4f865edb8
CaueFoyth <foythcaue@gmail.com>
2024-01-12 20:32:08 -0300

    Aula 1

--- primeiro_projeto/src/main.java
+++
@@ -1,8 +0,0 @@
-package primeiro_projeto.src;
-
-public class main {
-    public static void main(String[] args) {
-        System.out.println("Alô Mundo!");
-    }
-
-}
---
+++ primeiroprojeto/primeiroprojeto/src/App.java
@@ -0,0 +1,22 @@
+class Perfil {
+    public static void main(String[] args) {
+      System.out.println("Olá, Cauê");
+      System.out.println("Tudo bem?");
+    }
+  }
+
+class Soma{
+
+    public static void main(String[] args) {
+        System.out.println("Essa é a soma de 10 + 5");
+        System.out.println(10 + 5);
+    }
+}
+
+class Subtração{
+
+    public static void main(String[] args) {
+        System.out.println("Essa é a subtração de 10 + 5");
+        System.out.println(10 - 5);
+    }
+}

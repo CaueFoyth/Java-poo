2c30a0ae47dbebb955d61f7c795ba18a729bb697
CaueFoyth <foythcaue@gmail.com>
2024-01-14 22:25:55 -0300

    condicionais

---
+++ primeiroprojeto/src/condicional.java
@@ -0,0 +1,52 @@
+public class condicional {
+    public static void main(String[] args) {
+        int ano = 2022;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        String tipoPlano = "plus";
+
+        if (ano >= 2022) {
+            System.out.println("Filme Lançamento");
+        } else {
+            System.out.println("Filme Retro");
+        }
+
+        if (incluidoNoPlano || tipoPlano.equals("plus")) {
+            System.out.println("Filme liberado");
+        } else {
+            System.out.println("Deve pagar a locação");
+        }
+
+        int dia = 3;
+        String nomeDia;
+
+        switch (dia) {
+            case 1:
+                nomeDia = "domingo";
+                break;
+            case 2:
+                nomeDia = "segunda-feira";
+                break;
+            case 3:
+                nomeDia = "terça-feira";
+                break;
+            case 4:
+                nomeDia = "quarta-feira";
+                break;
+            case 5:
+                nomeDia = "quinta-feira";
+                break;
+            case 6:
+                nomeDia = "sexta-feira";
+                break;
+            case 7:
+                nomeDia = "sábado";
+                break;
+            default:
+                nomeDia = "Dia inválido";
+                break;
+        }
+
+        System.out.println("O dia " + dia + " é " + nomeDia);
+    }
+}
